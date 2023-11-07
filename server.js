8a4ba389b2ac4e6e44501e22e0eaf44af43f9b85
davidecardelli <davide.cardelli.94@gmail.com>
2023-11-07 16:13:29 +0100

    end bonus

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ dotenv.config();
 // ! default 3000, ma posso impostarla anche tramite .env
 port = process.env.PORT || 3000;
 
+// ! creo l'array con frasi
+const phrases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+];
+
+// ! creo una funzione che peschi una frase random
+function getRandomPhrase() {
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    return phrases[randomIndex];
+}
+
 // ! preparo la funzione per rispondere tramite html
 function htmlResponse(res, content) {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -16,9 +30,9 @@ function htmlResponse(res, content) {
 
 // ! creo il server
 const server = http.createServer(function (req, res) {
-    htmlResponse(res, "<h1>Hi, you are using the port " + port + "</h1>");
+    const randomPhrase = getRandomPhrase();
+    htmlResponse(res, "<h1>" + randomPhrase + "</h1>");
 });
-
 // ! lo avvio
 server.listen(port, function () {
     console.log("Server attivo su: http://localhost:" + port);
